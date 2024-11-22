a5dfff6744750109836d290a1a158a45d54ad06f
atldaveo <43223783+atldaveo@users.noreply.github.com>
2024-11-22 18:03:36 -0500

    Delete scripts directory

--- scripts/adoc_to_md.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT="output.xml"
-
-asciidoc -b docbook -o $OUTPUT $1
-pandoc -f docbook -t markdown_strict $OUTPUT -o $2
-rm $OUTPUT
--- scripts/zip.sh
+++
@@ -1,3 +0,0 @@
-find . -type d -name "practical_exercises" | while read dir; do
-    (cd "$dir" && zip -r "../${dir##*/}.zip" .)
-done
